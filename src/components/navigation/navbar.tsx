89ae99e5e1514b6533ac538b47deaecdf2baaa21
unknown <michaelxaviercanonizado@gmail.com>
2024-05-26 00:23:05 +0800

    feat: add navbar

---
+++ src/components/navigation/navbar.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+
+const Navbar = () => {
+	return (
+		<header
+			className={cn(
+				'sticky top-0 w-screen z-50 border-b-[1px] border-gray-200'
+			)}
+		>
+			<nav
+				className={cn('mx-auto px-4 py-2 flex justify-between items-center')}
+			>
+				<div>
+					<span className="text-2xl">Navbar</span>
+				</div>
+				<div>
+					<Link href="/">About</Link>
+				</div>
+			</nav>
+		</header>
+	);
+};
+
+export default Navbar;
