bdbaa926da0b60a6eea2b4351b6d8d7cf80d1653
unknown <michaelxaviercanonizado@gmail.com>
2024-05-29 00:39:45 +0800

    feat: add pop-up effect on card

--- src/components/content/card-type-b.tsx
+++ src/components/content/card-type-b.tsx
@@ -1,7 +1,11 @@
+"use client"
+
 import React from 'react';
 
 import Image from 'next/image';
 import { bayon } from '@/styles/fonts';
+
+import { motion } from 'framer-motion';
 
 import {
 	Card,
@@ -25,46 +29,68 @@ const CardTypeB = ({
 	description: string[];
 	color: string;
 }) => {
+	const cardVariants = {
+		hidden: {
+			y: 100,
+		},
+		visible: {
+			y: 0,
+		},
+	};
+
 	return (
-		<Card
-			className={cn(
+		<motion.div 
+			className="h-full"
+			variants={cardVariants}
+			initial="hidden"
+			whileInView="visible"
+			viewport={{ once: true }}
+			transition={{
+				type: 'spring',
+				ease: 'linear',
+				stiffness: 120,
+				y: { delay: 0.05 },
+			}}>
+			<Card
+				className={cn(
 				'bg-transparent border-none flex flex-col h-full',
 				className
-			)}
-		>
-			<div className="bg-transparent h-[200px] relative">
-				<div className="absolute z-10 bottom-[-50%] translate-x-[50%] w-[50%] aspect-square rounded-md overflow-hidden">
-					<Image
-						src={imagePath}
-						alt={title}
-						fill
-						style={{
-							objectFit: 'cover',
-							objectPosition: 'center',
-						}}
-					/>
-				</div>
-			</div>
-			<div
-				className="py-4 px-12 pt-24 bg-background-100 rounded-xl overflow-hidden shadow border-b-[20px] border-x-0 border-t-0 grow"
-				style={{
-					borderColor: color,
-				}}
+				)}
 			>
-				<CardHeader className="pt-4 pb-2 px-0">
-					<CardTitle
-						className={`${bayon.variable} text-foreground font-display tracking-widest text-2xl`}
-					>
-						{title}
-					</CardTitle>
-				</CardHeader>
-				<CardContent className="pt-0 px-0 ">
-					<CardDescription className="text-lg">
-						{description}
-					</CardDescription>
-				</CardContent>
-			</div>
-		</Card>
+				<div className="bg-transparent h-[200px] relative">
+					<div className="absolute z-10 bottom-[-50%] translate-x-[50%] w-[50%] aspect-square rounded-md overflow-hidden">
+						<Image
+							src={imagePath}
+							alt={title}
+							fill
+							style={{
+								objectFit: 'cover',
+								objectPosition: 'center',
+							}}
+						/>
+					</div>
+				</div>
+				<div
+					className="py-4 px-12 pt-24 bg-background-100 rounded-xl overflow-hidden shadow border-b-[20px] border-x-0 border-t-0 grow"
+					style={{
+						borderColor: color,
+					}}
+				>
+					<CardHeader className="pt-4 pb-2 px-0">
+						<CardTitle
+							className={`${bayon.variable} text-foreground font-display tracking-widest text-2xl`}
+						>
+							{title}
+						</CardTitle>
+					</CardHeader>
+					<CardContent className="pt-0 px-0 ">
+						<CardDescription className="text-lg">
+							{description}
+						</CardDescription>
+					</CardContent>
+				</div>
+			</Card>
+		</motion.div>
 	);
 };
 
